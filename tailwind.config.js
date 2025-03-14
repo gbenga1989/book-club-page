9b97c2755d87aed22dacc1fa270b91b397f9b6c1
gbenga <odetoyinbog@gmail.com>
2025-03-14 14:17:34 +0100

    14-5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+
     extend: {
       colors:{
        neu9: '#062630',
