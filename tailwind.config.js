0c23d453d80733f27d8dd467376540cd8751857f
gbenga <odetoyinbog@gmail.com>
2025-03-14 11:07:57 +0100

    14-1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+       neu9: '#062630',
+       neu7: '#385159 ',
+       neu2: '#E6E1DF',
+       neu1: '#FAF5F3',
+       lightsal5: '#FEA36F', 
+       lightsal1: '#FFE2D1', 
+       lightsal50: '#FFF5EF', 
+      },
+    }, 
+
+    extend: {
+      backgroundImage: {
+        'gradient-custom': "linear-gradient(107deg, #FF9A60 -11.37%, #062630 61.84%)",
+      },
+    }, 
+  },
+  plugins: [],
+}
